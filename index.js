1795133e906a0e8f9d73865408be3a9c4bfec95c
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-01-21 16:35:39 +0700

    add jsx-a11y plugin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     "./base",
     "./stylistic",
+    "./jsx",
   ],
   plugins: [
     "import",
---
+++ jsx.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "plugin:jsx-a11y/recommended",
+  ],
+  plugins: [
+    "jsx-a11y",
+  ],
+  rules: {
+    "jsx-a11y/no-autofocus": "off",
+  },
+};
