51955f3cbddd5e430c04e87ffaee5c7bc64a07f0
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-08-15 17:10:24 +0700

    remove temp fix for unused-imports plugin types

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,3 @@
-declare module "eslint-plugin-unused-imports" {
-  const value: unknown;
-  export default value;
-}
-
 declare module "eslint-plugin-react" {
   const value: unknown;
   export default value;
