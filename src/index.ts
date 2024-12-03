42234dd2cfd9e110d12f79208d36a42c7f9f4328
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-12-03 18:01:50 +0700

    feat: make jsx-a11y plugin optional

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import base from "./base.js";
 import importPlugin from "./import.js";
-import jsxAccessibility from "./jsx-a11y.js";
+import { loadJsxAccessibilityConfig } from "./jsx-a11y.js";
 import { loadReactConfig } from "./react.js";
 import stylistic from "./stylistic.js";
 import typescript from "./typescript.js";
@@ -12,7 +12,7 @@ export default [
   ...stylistic,
   ...typescript,
   ...(await loadReactConfig()),
-  ...jsxAccessibility,
+  ...(await loadJsxAccessibilityConfig()),
   ...importPlugin,
   ...unusedImports,
 ] as Linter.Config[];
--- src/jsx-a11y.ts
+++ src/jsx-a11y.ts
@@ -1,16 +1,22 @@
-import jsxA11y from "eslint-plugin-jsx-a11y";
 import type { Linter } from "eslint";
 
-export default [
-  {
-    files: ["src/**/*.+(jsx|tsx)", "./*.+(jsx|tsx)"],
-    ignores: ["node_modules/"],
-    plugins: {
-      "jsx-a11y": jsxA11y,
-    },
-    rules: {
-      ...jsxA11y.flatConfigs.recommended.rules,
-      "jsx-a11y/no-autofocus": "off",
-    },
-  },
-] as Linter.Config[];
+export async function loadJsxAccessibilityConfig() {
+  try {
+    const { default: jsxA11y } = await import("eslint-plugin-jsx-a11y");
+    return [
+      {
+        files: ["src/**/*.+(jsx|tsx)", "./*.+(jsx|tsx)"],
+        ignores: ["node_modules/"],
+        plugins: {
+          "jsx-a11y": jsxA11y,
+        },
+        rules: {
+          ...jsxA11y.flatConfigs.recommended.rules,
+          "jsx-a11y/no-autofocus": "off",
+        },
+      },
+    ] as Linter.Config[];
+  } catch {
+    return [];
+  }
+}
